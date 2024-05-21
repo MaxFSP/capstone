b38ece50ebd208a6866d42dd6f83aa7a8cca7703
Max <santiago.farfan2001@gmail.com>
2024-05-21 11:04:30 -0400

    Setup

--- next.config.js
+++ next.config.js
@@ -3,8 +3,44 @@
  * for Docker builds.
  */
 await import("./src/env.js");
+import nextPWA from "next-pwa";
 
-/** @type {import("next").NextConfig} */
-const config = {};
+/** @type {import('next').NextConfig} */
 
-export default config;
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+};
+
+const withPWA = nextPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+// Export the combined configuration for Next.js with PWA support
+export default withPWA(nextConfig);
+
+// /** @type {import("next").NextConfig} */
+// const config = {
+//   reactStrictMode: true, // Enable React strict mode for improved error handling
+//   swcMinify: true, // Enable SWC minification for improved performance
+//   compiler: {
+//     removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+//   },
+// };
+
+// // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+// const withPWA = nextPWA({
+//   dest: "public",
+//   disable: process.env.NODE_ENV === "development", // Disable PWA in development
+//   register: true, // Register the PWA service worker
+//   skipWaiting: true, // Skip waiting for service worker activation
+// });
+
+// // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+// export default withPWA(config);
